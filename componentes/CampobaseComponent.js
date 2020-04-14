fc11a241bc69330488612163e6fa419d9a77778b
Jokin Sanchez <jokin.s97@gmail.com>
2020-04-14 19:11:12 +0200

    Drawer Navigation

--- componentes/CampobaseComponent.js
+++ componentes/CampobaseComponent.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Calendario from './CalendarioComponent';
 import DetalleExcursion from './DetalleExcursionComponent';
 import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 function CalendarioNavegador() {
   return (
     <Stack.Navigator
       initialRouteName="Calendario"
-      headerMode="screen"
+      headerMode="float"
       screenOptions={{
         headerTintColor: '#fff',
         headerStyle: { backgroundColor: '#015afc' },
@@ -32,19 +35,55 @@ function CalendarioNavegador() {
           title: 'Detalle Excursión',
         }}
       />
+
     </Stack.Navigator>
+  );
+}
+
+function HomeNavegador() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      headerMode="float"
+      screenOptions={{
+        headerTintColor: '#fff',
+        headerStyle: { backgroundColor: '#015afc' }, headerTitleStyle: { color: '#fff' },
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          title: 'Campo Base',
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function DrawerNavegador() {
+  return (
+    <Drawer.Navigator
+      drawerStyle={{
+        backgroundColor: '#c2d3da',
+      }}
+      initialRouteName="Home"
+    >
+      <Drawer.Screen name="Home" component={HomeNavegador} />
+      <Drawer.Screen name="Calendario" component={CalendarioNavegador} />
+    </Drawer.Navigator>
   );
 }
 
 class Campobase extends Component {
 
   render() {
- 
+
     return (
       <NavigationContainer>
-        <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
-          <CalendarioNavegador />
-        </View>      
+        <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
+          <DrawerNavegador />
+        </View>
       </NavigationContainer>
     );
   }
